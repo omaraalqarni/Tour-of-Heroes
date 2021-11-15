af4b88ff057feb48cec69393f97c6d1d082f2038
omarqr0 <pleess30@gmail.com>
2021-11-15 15:33:06 +0300

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import {Hero} from "./hero";
+
+export const HEROES : Hero[] = [
+    {id: 2, name: "Spider-man"},
+    {id:3, name: "Omar-man"}, 
+    {id:4, name: "Batman"},
+    {id:5, name: "Super-man"},
+    {id:6, name: "Amazing-man"}
+]
